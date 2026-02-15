e004cd239542f5d6a7e7023cdb6d96a9d6462614
JoannaZhang-cs <jzsunset10@gmail.com>
2026-02-15 13:44:44 -0500

    Update PizzaBuilder.vue

--- vue-project/src/views/PizzaBuilder.vue
+++ vue-project/src/views/PizzaBuilder.vue
@@ -1,12 +1,13 @@
 <template>
-    <div class = "container">
-        <AnimalCard v-for="animal in animals" :key = "animal.name" :animal="animal">{{ animal.name }}</AnimalCard>
-        <!-- animal card makes a card for every animal -->
-         <!-- left side is name/prop and right side is what you're passing in -->
+  <div class="container">
+    <AnimalCard v-for="animal in animals" :key="animal.name" :animal="animal">{{
+      animal.name
+    }}</AnimalCard>
+    <!-- animal card makes a card for every animal -->
+    <!-- left side is name/prop and right side is what you're passing in -->
 
-
-<!-- now u can see every animals name on the page -->
-    </div>
+    <!-- now u can see every animals name on the page -->
+  </div>
 </template>
 
 <script setup>
@@ -14,16 +15,16 @@ import { ref } from 'vue'
 import PizzaBuilder from '@/components/PizzaBuilder.vue'
 //don't need ref here because array isn't being updated
 const toppings = ref([
-  { name: 'sauce', price:  },
-  { name: 'cheese', price:  },
-  { name: 'mushroom',price: },
-  { name: 'jalapeno',price: },
-  { name: 'onion',price: },
-  { name: 'pepperoni', price: },
-  { name: 'pineapple', price:  },
-  { name: 'redpepper', price: },
-  { name: 'sausage', price:  },
-  { name: 'salmon', price: },
+  { name: 'sauce', price: 20 },
+  { name: 'cheese', price: 30 },
+  { name: 'mushroom', price: 40 },
+  { name: 'jalapeno', price: 40 },
+  { name: 'onion', price: 40 },
+  { name: 'pepperoni', price: 40 },
+  { name: 'pineapple', price: 40 },
+  { name: 'redpepper', price: 40 },
+  { name: 'sausage', price: 50 },
+  { name: 'salmon', price: 50 },
 ])
 </script>
 
