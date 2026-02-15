d6fd04a43401b9f2b02c83ecfe27e33121006ebc
JoannaZhang-cs <jzsunset10@gmail.com>
2026-02-15 14:09:47 -0500

    add

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,14 +1,11 @@
-<script setup>
-const array = []
-//toppings = []
-</script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div>
+    <RouterView />
+  </div>
 </template>
+
+<script setup>
+const toppings = []
+</script>
 
 <style scoped></style>
