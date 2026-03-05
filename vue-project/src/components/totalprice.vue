f8e8178786305f584fad46d63f5d05a7f7316bda
JoannaZhang-cs <jzsunset10@gmail.com>
2026-03-05 12:16:40 -0500

    Update totalprice.vue

--- vue-project/src/components/totalprice.vue
+++ vue-project/src/components/totalprice.vue
@@ -1,7 +1,10 @@
 <template>
-    <div>
-
+    <div class="container">
+        <TotalPrice v-for="price in toppings" :key = "topping.price" :price="price">{{ topping.price }}
+          <button @click="addToCart(price)">Add to Cart</button>
+        </TotalPrice>
     </div>
+
 </template>
 
 <script setup>
